8bbb0ac0c60f377aa758d4722739f4cc314c674a
Utkarsh Mani Tripathi <utkarshmani1997@gmail.com>
2017-05-17 13:48:16 +0530

    Updated with few modification and solved the issue

--- exercise_1/ex1_multi.py
+++ exercise_1/ex1_multi.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 """
-Machine Learning Online Class
+Machine Learning Online Class Coursera
 Exercise 1: Linear regression with multiple variables
+Modified with few changes,Solved the issue
 """
 
 # Initialization
@@ -20,20 +21,21 @@ print('Loading data ...','\n')
 ## Load Data
 print('Plotting Data ...','\n')
 
-data = pd.read_csv("ex1data2.txt",names=["size","bedrooms","price"])
-s = np.array(data.size)
-r = np.array(data.bedrooms)
+data = pd.read_csv("ex1data2.txt",names=["size1","bedrooms","price"])
+s = np.array(data.size1)
+b = np.array(data.bedrooms)
 p = np.array(data.price)
-m = len(r) # number of training examples
+m = len(b) # number of training examples
 
 # Design Matrix
 s = np.vstack(s)
-r = np.vstack(r)
-X = np.hstack((s,r))
+b = np.vstack(b)
+X = np.hstack((s,b))
+
 
 # Print out some data points
 print('First 10 examples from the dataset: \n')
-print(" size = ", s[:10],"\n"," bedrooms = ", r[:10], "\n")
+print(" size = ", s[:10],"\n"," bedrooms = ", b[:10], "\n")
 
 input('Program paused. Press enter to continue.\n')
 
@@ -71,15 +73,17 @@ plt.show()
 print('Theta computed from gradient descent: \n')
 print(theta,'\n')
 
-# Estimate the price of a 1650 sq-ft, 3 br house
+# Estimating the price of a house on the given size in square feet and no of bedrooms
+siz=int(input("Program paused. Enter the size of house in sq-ft:"))
+br=int(input("Program paused. Enter the no of bedrooms:"))
 
 # Recall that the first column of X is all-ones. Thus, it does
 # not need to be normalized.
-normalized_specs = np.array([1,((1650-s.mean())/s.std()),((3-r.mean())/r.std())])
+normalized_specs = np.array([1,((siz-s.mean())/s.std()),((br-b.mean())/b.std())])
 price = np.dot(normalized_specs,theta) 
 
 
-print('Predicted price of a 1650 sq-ft, 3 br house (using gradient descent):\n ',
+print('Predicted price of house with size = {} and {} no of bedrooms (using gradient descent):\n '.format(siz, br),
       price)
 
 input('Program paused. Press enter to continue.\n')
@@ -89,14 +93,14 @@ print('Solving with normal equations...\n')
 
 data = pd.read_csv("ex1data2.txt",names=["sz","bed","price"])
 s = np.array(data.sz)
-r = np.array(data.bed)
+b = np.array(data.bed)
 p = np.array(data.price)
-m = len(r) # number of training examples
+m = len(b) # number of training examples
 
 # Design Matrix
 s = np.vstack(s)
-r = np.vstack(r)
-X = np.hstack((s,r))
+b = np.vstack(b)
+X = np.hstack((s,b))
 
 # Add intercept term to X
 X = np.hstack((np.ones_like(s),X))
@@ -109,11 +113,12 @@ print('Theta computed from the normal equations: \n')
 print(theta)
 print('\n')
 
-# Estimate the price of a 1650 sq-ft, 3 br house
-price = np.dot([1,1650,3],theta) # You should change this
+# Estimate the price of a house, by Normal equations
+price = np.dot([1,siz,br],theta) # You should change this
 
 
-print('Predicted price of a 1650 sq-ft, 3 br house (using normal equations): \n',
-       price)
+print('Predicted price of house with size = {} and {} no of bedrooms (using gradient descent):\n '.format(siz, br),
+      price)
+
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
