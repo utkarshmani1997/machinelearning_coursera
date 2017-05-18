c585de6533b23f2e0e6505008860de93f28e9ef7
Utkarsh Mani Tripathi <utkarshmani1997@gmail.com>
2017-05-18 15:23:25 +0530

    Updated with standard input/Output feature

--- exercise_2/ex2.py
+++ exercise_2/ex2.py
@@ -1,38 +1,52 @@
 """
-Machine Learning Online Class - Exercise 2: Logistic Regression
+Machine Learning Online Class Coursera
+Exercise 1: Logistic regression with multiple variables
+Modified with few changes,Solved the issue
+Modified by github.com/utkarshmani1997
 """
+# depends on 
+#     sigmoid.py
+#     costFunction.py
+#     predict.py
+#     plotData.py
+#     plotDecisionBoundary.py
 
 ## Initialization
-import pandas as pd
-import numpy as np
-from scipy.optimize import minimize
-from ex2_utils import *
 
+import sigmoid as sig
+import costFunction as cf
+import predict as pr
+import plotData as pd
+import plotDecisionBoundary as pdb
+import numpy as np 
+from scipy.optimize import fmin
+from scipy.optimize import fmin_bfgs
 
 ## Load Data
 #  The first two columns contains the exam scores and the third column
 #  contains the label.
 
-data = pd.read_csv('ex2data1.txt', names=['x1','x2','y'])
-X = np.asarray(data[["x1","x2"]])
-y = np.asarray(data["y"])
+data = np.loadtxt('ex2data1.txt', delimiter=",")
+X = data[:,:2]
+y = data[:,2]
 
 ## ==================== Part 1: Plotting ====================
 #  We start the exercise by first plotting the data to understand the 
 #  the problem we are working with.
 
-print("Plotting data with + indicating (y = 1) examples and o indicating",
-" (y =0) examples.")
+print('Plotting data with + indicating (y = 1) examples and o indicating (y = 0) examples.')
 
-fig, ax = plotData(X, y)
+plt, p1, p2 = pd.plotData(X, y)
 
-# Specified in plot order
-ax.legend(['Admitted', 'Not admitted'])
+# # Labels and Legend
+plt.xlabel('Exam 1 score')
+plt.ylabel('Exam 2 score')
+plt.legend((p1, p2), ('Admitted', 'Not Admitted'), numpoints=1, handlelength=0)
 
+plt.show(block=False) # prevents having to close the graph to move forward with ex2.py
 
-fig.show()
-
-input('\nProgram paused. Press enter to continue.\n')
+input('Program paused. Press enter to continue.\n')
+# plt.close()
 
 ## ============ Part 2: Compute Cost and Gradient ============
 #  In this part of the exercise, you will implement the cost and gradient
@@ -40,69 +54,66 @@ input('\nProgram paused. Press enter to continue.\n')
 #  costFunction.m
 
 #  Setup the data matrix appropriately, and add ones for the intercept term
-
-# Add intercept term to x and X_test
-X = np.hstack((np.ones_like(y)[:,None],X))
+m,n = X.shape
+X_padded = np.column_stack((np.ones((m,1)), X)) 
 
 # Initialize fitting parameters
-initial_theta = np.zeros(3)
+initial_theta = np.zeros((n + 1, 1))
 
 # Compute and display initial cost and gradient
-cost, grad = costFunction(initial_theta, X, y)
+cost, grad = cf.costFunction(initial_theta, X_padded, y, return_grad=True)
 
-print('Cost at initial theta (zeros): \n', cost)
-print('Gradient at initial theta (zeros): \n',grad)
+print('Cost at initial theta (zeros): {:f}'.format(cost))
+print('Gradient at initial theta (zeros):')
+print(grad)
 
-input('\nProgram paused. Press enter to continue.')
+input('Program paused. Press enter to continue.\n')
 
 
-## ============= Part 3: Optimizing using fminunc  =============
-#  In this exercise, you will use a built-in function (fminunc) to find the
+## ============= Part 3: Optimizing using fmin (and fmin_bfgs)  =============
+#  In this exercise, you will use a built-in function (fmin) to find the
 #  optimal parameters theta.
 
-res = minimize(costFunction,
-	       initial_theta,
-	       method='Newton-CG',
-	       args=(X,y),
-	       jac=True, 
-	       options={'maxiter':400,
-			'disp':True})
-
-theta = res.x
+#  Run fmin and fmin_bfgs to obtain the optimal theta
+#  This function will return theta and the cost 
+#  fmin followed by fmin_bfgs inspired by stackoverflow.com/a/23089696/583834
+#  overkill... but wanted to use fmin_bfgs, and got error if used first
+myargs=(X_padded, y)
+theta = fmin(cf.costFunction, x0=initial_theta, args=myargs)
+theta, cost_at_theta, _, _, _, _, _ = fmin_bfgs(cf.costFunction, x0=theta, args=myargs, full_output=True)
 
 # Print theta to screen
-print('Cost at theta found by minimize: \n', res.fun)
-print('theta: \n', theta)
-
+print('Cost at theta found by fmin: {:f}'.format(cost_at_theta))
+print('theta:'),
+print(theta)
 
 # Plot Boundary
-plotDecisionBoundary(theta, X, y)
+pdb.plotDecisionBoundary(theta, X_padded, y)
 
+plt.hold(False) # prevents further drawing on plot
+plt.show(block=False) 
 
-input('\nProgram paused. Press enter to continue.\n')
+input('Program paused. Press enter to continue.\n')
+
 
 ## ============== Part 4: Predict and Accuracies ==============
 #  After learning the parameters, you'll like to use it to predict the outcomes
 #  on unseen data. In this part, you will use the logistic regression model
-#  to predict the probability that a student with score 45 on exam 1 and 
-#  score 85 on exam 2 will be admitted.
-#
+#  to predict the probability that a student with the score getting on
+#  exam 1 and on exam 2 will be admitted.
 #  Furthermore, you will compute the training and test set accuracies of 
 #  our model.
 #
-#  Your task is to complete the code in predict.m
-
-#  Predict probability for a student with score 45 on exam 1 
-#  and score 85 on exam 2 
-
-prob = sigmoid(np.dot([1,45,85],theta))
-print('For a student with scores 45 and 85, we predict an ',
-      'admission probability of ', prob)
+#  Predict probability for a student with given score on exam 1 
+#  and exam 2 
+sc1=int(input("Program paused. Enter the score of Exam 1 : "))
+sc2=int(input("Program paused. Enter the score of Exam 2 : "))
+prob = sig.sigmoid(np.dot(np.array([1,sc1,sc2]),theta))
+print('For a student with scores {} and {}, we predict an admission probability of {:f}'.format(sc1,sc2,prob))
 
 # Compute accuracy on our training set
-p = predict(theta, X)
+p = pr.predict(theta, X_padded)
 
-print('Train Accuracy: \n', np.mean(p==y)*100)
+print('Train Accuracy: {:f}'.format(np.mean(p == y) * 100))
 
 input('Program paused. Press enter to continue.\n')
-
